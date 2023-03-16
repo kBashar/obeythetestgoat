c30fc9279593764c1c1944c4ceb6441c0e00ce74
kBashar <khyrulbashar.0.1@gmail.com>
2023-03-16 15:01:42 +0600

    Model Item added

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from lists.models import Item
 
 class HomePageTest(TestCase):
     def setUp(self) -> None:
@@ -19,4 +20,29 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': test_data})
         self.assertIn(test_data, response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
-        
+
+class ItemModelTest(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        return super().tearDown()
+    
+    def test_saving_and_retriving_item(self):
+
+        item_1 = Item()
+        item_1.text = 'First Item'
+        item_1.save()
+
+        item_2 = Item()
+        item_2.text = 'Second Item'
+        item_2.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(2, saved_items.count())
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual('First Item', first_saved_item.text)
+        self.assertEqual('Second Item', second_saved_item.text)
+
