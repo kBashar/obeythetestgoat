c4aceff8e087b5c351c233570aeec52d1a44c325
kBashar <khyrulbashar.0.1@gmail.com>
2023-04-08 17:55:35 +0600

    make debug and production settings separate

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,13 +19,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x@9g)3^sk+xy-1$b+2+98rgy!=%iy_ef^!!2+g7ogrv5a^1_$d'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+if 'DJANGO_DEBUG_FALSE' in os.environ:  
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']  
+    ALLOWED_HOSTS = [os.environ['SITENAME']]  
+else:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'x@9g)3^sk+xy-1$b+2+98rgy!=%iy_ef^!!2+g7ogrv5a^1_$d'
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
