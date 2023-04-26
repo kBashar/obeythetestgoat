f6c5879f009fb21e61a75f507a05f675f9e1b1eb
kBashar <khyrulbashar.0.1@gmail.com>
2023-04-27 01:45:54 +0600

    FTs are distributed to their separate files

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.keys import Keys
+from .base import FunctionalTest
+
+class LayoutAndStylingTest(FunctionalTest):
+
+     def test_layout_and_styling(self):
+        # edith visits the page
+
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # check if the input box is in the middle 
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,7 @@
+from unittest import skip
+from .base import FunctionalTest
+
+class ItemValidationTest(FunctionalTest):
+
+    def test_can_not_add_empty_list_item(self):
+        self.fail("write me!")
