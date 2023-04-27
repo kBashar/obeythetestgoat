7a5712a8825eaadd536857d2e81ef61059b44518
kBashar <khyrulbashar.0.1@gmail.com>
2023-04-27 13:49:50 +0600

    unit test and functional test are all splitted in different files

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -17,6 +17,23 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
     
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    # def wait_for_row_in_table(self, row_text):
+    #     table = self.browser.find_element_by_id('id_list_table')
+    #     rows = table.find_elements_by_tag_name('tr')
+    #     self.wait_for(lambda: self.assertIn(row_text, [row.text for row in rows]))
+    #     return
+
+
     def wait_for_row_in_table(self, row_text):
         start_time = time.time()
         while True:
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
-from django.http import HttpRequest
 from lists.models import Item, List
 
 class HomePageTest(TestCase):
@@ -65,41 +62,6 @@ class ListViewTest(TestCase):
 
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
-
-class ListAndItemModelTest(TestCase):
-    def setUp(self) -> None:
-        return super().setUp()
-    
-    def tearDown(self) -> None:
-        return super().tearDown()
-    
-    def test_saving_and_retriving_item(self):
-        list_ = List()
-        list_.save()
-
-        item_1 = Item()
-        item_1.text = 'First Item'
-        item_1.list = list_
-        item_1.save()
-
-        item_2 = Item()
-        item_2.text = 'Second Item'
-        item_2.list = list_
-        item_2.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(2, saved_items.count())
-
-        first_saved_item = saved_items[0]
-        self.assertEqual('First Item', first_saved_item.text)
-        self.assertEqual(first_saved_item.list, list_)
-
-        second_saved_item = saved_items[1]
-        self.assertEqual('Second Item', second_saved_item.text)
-        self.assertEqual(second_saved_item.list, list_)
 
 class NewItemTest(TestCase):
 
